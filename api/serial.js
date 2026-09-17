7cea6ac70704c880ce788bcc054de63eaac7eeec
albertencia <albertvieirabiopago@gmail.com>
2026-09-17 12:04:52 -0400

    Update serial.js

--- api/serial.js
+++ api/serial.js
@@ -1,5 +1,21 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    mensaje: "Vercel funcionando"
-  });
+
+  try {
+
+    const response = await fetch(
+      "https://erp.ex-cle.com/api/method/ping"
+    );
+
+    const data = await response.text();
+
+    res.status(200).send(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 }
